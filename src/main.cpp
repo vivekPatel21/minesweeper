cc833487f395ada79bdd1398f98c80cf4e2d9c9b
Vivek Patel <vpatel20001@gmail.com>
2024-06-14 18:03:13 -0400

    libraries work

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+// Eigen
+#include <Eigen/Dense>
+
+// GLFW
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+
+
+// stb_image
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
+//QT
+#include <qtconfiginclude.h>
+
+//
+#include <oaidl.h>
+
+int main(int, char**){
+    printf("Hello, from Minesweeper!\n");
+}
